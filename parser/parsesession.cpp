9d24c25f12bff80881c106eb48cd9d494a9d487a
Niko Sams <niko.sams@gmail.com>
2009-03-14 14:26:20 +0000

    fix include paths, patch by scarabeus

--- parser/parsesession.cpp
+++ parser/parsesession.cpp
@@ -22,7 +22,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION     *
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.           *
  *****************************************************************************/
-#include "parser/parsesession.h"
+#include "parsesession.h"
 
 #include "kdev-pg-memory-pool.h"
 #include "kdev-pg-token-stream.h"
