eb7dc077d0634143b33f375662fb6aaf8f7393e5
Milian Wolff <mail@milianw.de>
2009-08-05 14:29:34 +0000

    make tokenize testscript work with code from the CLI

--- parser/test/test-tokenize.php
+++ parser/test/test-tokenize.php
@@ -2,19 +2,23 @@
 //compares the tokens returned by token_get_all with the lexer
 
 if (!isset($_SERVER['argv'][1])) {
-    die("Usage: {$_SERVER['argv'][0]} file.php\n");
+    die("Usage: {$_SERVER['argv'][0]} [FILE | --code CODE]\n");
 }
 
-$file = $_SERVER['argv'][1];
-unset($_SERVER['argv'][0]);
-foreach ($_SERVER['argv'] as $file) {
-    processFile($file);
+if ( $_SERVER['argv'][1] == '--code' ) {
+    echo "processing code:\n\n";
+    processString($_SERVER['argv'][2]);
+} else {
+    unset($_SERVER['argv'][0]);
+    foreach ($_SERVER['argv'] as $file) {
+        echo "processing file $file:\n\n";
+        processString(file_get_contents($file));
+    }
 }
 
-function processFile($file) {
+function processString($c) {
     $tokens = array();
     $start = microtime(true);
-    $c = file_get_contents($file);
     $c = str_replace("\r", "", $c);
     foreach (token_get_all($c) as $i) {
         if (is_string($i)) {
@@ -34,7 +38,7 @@ function processFile($file) {
 
     $out = array();
     $start = microtime(true);
-    exec("php-parser --print-tokens $file", $out, $ret);
+    exec("php-parser --print-tokens --code ".escapeshellarg($c), $out, $ret);
     $parserTime = microtime(true) - $start;
     if ($ret != 0) {
         echo "php-parser failed\n";
@@ -46,13 +50,13 @@ function processFile($file) {
     array_pop($out); //remove "end of file"
     $diff = array_diff($tokens, $out);
     if (!$diff || (count($tokens) != count($out))) {
-        echo "$file correct tokenized ($parserTime / $phpTime)...\n";
+        echo "code correctly tokenized ($parserTime / $phpTime)...\n";
     } else {
         echo "******* parser output:\n";
         out($out);
         echo "******* expected:\n";
         out($tokens);
-        echo "******* differences in $file:\n";
+        echo "******* differences in code:\n";
         out($diff);
         exit(255);
     }
