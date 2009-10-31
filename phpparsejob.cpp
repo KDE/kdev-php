0f2842e77e2d42e94f8cbee78c9fc47b44dd8dc1
Milian Wolff <mail@milianw.de>
2009-10-31 20:51:34 +0000

    Copy UrlParseLock from CPPParseJob. Thankfully something like this was already written by someone - thanks to the author!
    Fixes parsing of phpMyAdmin on my machine. That crashed because the same file
    got parsed repetitivly at the same time (due to includes) and lead to crashes
    since the Context got deleted eventually, though a builder was still running.
    
    TODO: push the UrlParseLock in abstract form to KDevplatform

--- phpparsejob.cpp
+++ phpparsejob.cpp
@@ -54,11 +54,47 @@
 #include "phpducontext.h"
 
 #include <QtCore/QReadLocker>
+#include <QtCore/QThread>
 
 using namespace KDevelop;
 
 namespace Php
 {
+
+///TODO: push this into kdevplatform - copied from cpp for now
+
+///Facilities to prevent multiple parse-jobs from processing the same url.
+QMutex urlParseMutex;
+QMap<IndexedString, QPair<Qt::HANDLE, uint> > parsingUrls;
+
+struct UrlParseLock {
+    UrlParseLock(IndexedString url) : m_url(url) {
+        QMutexLocker lock(&urlParseMutex);
+        while (parsingUrls.contains(m_url) && parsingUrls[m_url].first != QThread::currentThreadId()) {
+            //Wait here until no other thread is updating parsing the url
+            lock.unlock();
+            sleep(1);
+            lock.relock();
+        }
+        if (parsingUrls.contains(m_url)) {
+            ++parsingUrls[m_url].second;
+        } else {
+            parsingUrls.insert(m_url, qMakePair(QThread::currentThreadId(), 1u));
+        }
+    }
+
+    ~UrlParseLock() {
+        QMutexLocker lock(&urlParseMutex);
+        Q_ASSERT(parsingUrls.contains(m_url));
+        Q_ASSERT(parsingUrls[m_url].first == QThread::currentThreadId());
+        --parsingUrls[m_url].second;
+        if (parsingUrls[m_url].second == 0) {
+            parsingUrls.remove(m_url);
+        }
+    }
+
+    IndexedString m_url;
+};
 
 ParseJob::ParseJob(const KUrl &url, QObject *parent)
         : KDevelop::ParseJob(url, parent)
@@ -97,6 +133,9 @@ void ParseJob::run()
         kDebug() << "waiting for InternalFunctions.php to finish parsing";
         QReadLocker(php()->internalFunctionsLock());
     }
+
+    UrlParseLock urlLock(document());
+
     {
         DUChainReadLocker lock(DUChain::lock());
         bool needsUpdate = true;
