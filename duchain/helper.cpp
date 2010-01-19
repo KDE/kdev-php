3f6fac98c28a1bc59c7ba72b8881ae2b4e926050
Milian Wolff <mail@milianw.de>
2010-01-19 22:51:23 +0000

    save us some dynamic casts in prettyName()

--- duchain/helper.cpp
+++ duchain/helper.cpp
@@ -283,12 +283,18 @@ IndexedString getIncludeFileForNode(UnaryExpressionAst* node, EditorIntegrator* 
 }
 
 QString prettyName(Declaration* dec) {
-    if ( ClassDeclaration* classDec = dynamic_cast<ClassDeclaration*>(dec) ) {
-        return classDec->prettyName();
-    } else if ( ClassMethodDeclaration* classMember = dynamic_cast<ClassMethodDeclaration*>(dec) ) {
+     if ( dec->context() && dec->context()->type() == DUContext::Class && dec->isFunctionDeclaration() ) {
+        ClassMethodDeclaration* classMember = dynamic_cast<ClassMethodDeclaration*>(dec);
+        Q_ASSERT(classMember);
         return classMember->prettyName();
-    } else if ( FunctionDeclaration* func = dynamic_cast<FunctionDeclaration*>(dec) ) {
+    } else if ( dec->isFunctionDeclaration() ) {
+        FunctionDeclaration* func = dynamic_cast<FunctionDeclaration*>(dec);
+        Q_ASSERT(func);
         return func->prettyName();
+    } else if ( dec->internalContext() && dec->internalContext()->type() == DUContext::Class ) {
+        ClassDeclaration* classDec = dynamic_cast<ClassDeclaration*>(dec);
+        Q_ASSERT(classDec);
+        return classDec->prettyName();
     } else {
         return dec->identifier().toString();
     }
